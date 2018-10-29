1e4f068a9e5e84a55c794fe62ec267cabe335944
Tamara <t.ceccato@campus.unimib.it>
2018-10-29 17:55:24 +0100

    added alignment for reads with more than one transcript

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <seqan/align.h>
 #include <stdio.h>
 #include <string>
 #include <vector> // std::vector
@@ -18,6 +19,7 @@ BF bloom(sizebloom);
 vector<int> search(const string &kmer, BF &bloomfilter) {
   return bloomfilter.get_index(kmer);
 }
+using namespace seqan;
 
 // STEP 1: declare the type of file handler and the read() function
 KSEQ_INIT(gzFile, gzread)
@@ -36,21 +38,21 @@ int main(int argc, char *argv[]) {
   vector<string> transcript_kmers_vec;
   BF bloom(sizebloom);
   string name_transcript;
-string transcript_name = "";
-string read_name = "";
+  string transcript_name = "";
+  string read_name = "";
+  typedef String<char> TSequence;             // sequence type
 
-if( argc > 1 ) {
-      transcript_name = argv[1];
-      read_name = argv[2];
-    }
-    else {
-      cout << "Error in input" << endl;
-      return 1;
-}
+  if (argc > 1) {
+    transcript_name = argv[1];
+    read_name = argv[2];
+  } else {
+    cout << "Error in input" << endl;
+    return 1;
+  }
 
   transcript_file =
       gzopen(transcript_name.c_str(), "r"); // STEP 2: open the file handler
-  seq = kseq_init(transcript_file);       // STEP 3: initialize seq
+  seq = kseq_init(transcript_file);         // STEP 3: initialize seq
 
   // open and read the .fa
   while ((file_line = kseq_read(seq)) >=
@@ -83,18 +85,16 @@ if( argc > 1 ) {
   kseq_destroy(seq);        // STEP 5: destroy seq
   gzclose(transcript_file); // STEP 6: close the file handler
 
-  
   bloom.switch_mode(1);
 
-transcript_file =
-      gzopen(transcript_name.c_str(), "r"); 
-  seq = kseq_init(transcript_file);      
+  transcript_file = gzopen(transcript_name.c_str(), "r");
+  seq = kseq_init(transcript_file);
   int idx = 0;
-  // open and read the .fa, every time a kmer is found the relative index is added to BF
-  while ((file_line = kseq_read(seq)) >=
-         0) { 
+  // open and read the .fa, every time a kmer is found the relative index is
+  // added to BF
+  while ((file_line = kseq_read(seq)) >= 0) {
     string input_seq;
-   
+
     // split each sequence of a transcritp in k-mer with k=n
     input_seq = seq->seq.s;
     transcript_kmers_vec.resize(input_seq.size() - (kmer_length - 1));
@@ -103,11 +103,10 @@ transcript_file =
               [kmer_length](const auto &i) { return string(&i, kmer_length); });
 
     // add for each k-mer its id to BF
-    for (auto &kmer : transcript_kmers_vec) 
-    	bloom.add_to_kmer(kmer, idx);
-    
-    idx++;
+    for (auto &kmer : transcript_kmers_vec)
+      bloom.add_to_kmer(kmer, idx);
 
+    idx++;
   }
 
   bloom.switch_mode(2);
@@ -118,13 +117,11 @@ transcript_file =
   map<int, int> classification_id;
   ofstream final_id;
   vector<int> id_kmer;
-  final_id.open("final_id.fa");
+  final_id.open("example/final_id.fa");
 
   // open .fq file that contains the reads
-  read_file =
-      gzopen(read_name.c_str(), "r"); // STEP 2: open the file handler
-  seq = kseq_init(read_file);               // STEP 3: initialize seq
-
+  read_file = gzopen(read_name.c_str(), "r"); // STEP 2: open the file handler
+  seq = kseq_init(read_file);                 // STEP 3: initialize seq
 
   while ((file_line = kseq_read(seq)) >= 0) { // STEP 4: read sequence of reads
 
@@ -169,7 +166,6 @@ transcript_file =
       }
     }
 
-
     classification_id.clear();
     id_kmer.clear();
     read_kmers_vec.clear();
@@ -180,6 +176,71 @@ transcript_file =
   printf("return value: %d\n", file_line);
   kseq_destroy(seq);  // STEP 5: destroy seq
   gzclose(read_file); // STEP 6: close the file handler
+
+
+  // read fasta created as output in the preious step
+  gzFile test_file;
+
+  test_file =
+      gzopen("example/final_id.fa", "r"); // STEP 2: open the file handler
+  seq = kseq_init(test_file);             // STEP 3: initialize seq
+
+  // seq1--> read
+  // seq2--> transcript
+
+  ofstream al_file;
+  al_file.open("example/alignment.fa");
+  while ((file_line = kseq_read(seq)) >= 0) { // read final_id
+    kseq_t *seq_tran;
+    string transcript_assigned;
+    transcript_assigned = seq->name.s;
+    TSequence seq1 = seq->seq.s;
+    gzFile transcript_global;
+    string name_tr;
+ 	TSequence seq2;
+
+    transcript_global = gzopen("example/chrY_mod.fa", "r");
+    seq_tran = kseq_init(transcript_global);
+    while ((file_line = kseq_read(seq_tran)) >= 0) { // read final_id
+      name_tr = seq_tran->name.s;
+      if (name_tr == transcript_assigned) {
+        seq2 = seq_tran->seq.s;
+        Align<String<char>> ali;
+        resize(rows(ali), 2);
+        assignSource(row(ali, 0), seq1);
+        assignSource(row(ali, 1), seq2);
+        al_file << ">" << name_tr << endl;
+        al_file << ";Score = "
+                << localAlignment(ali, Score<int>(3, -3, -2, -2), DynamicGaps())
+                << endl;
+        al_file << seq1 << endl;
+      }
+    }
+    kseq_destroy(seq_tran);     // STEP 5: destroy seq
+    gzclose(transcript_global); // STEP 6: close the file handler
+  }
+  al_file.close();
+  printf("return value: %d\n", file_line);
+  kseq_destroy(seq);  // STEP 5: destroy seq
+  gzclose(test_file); // STEP 6: close the file handler
+
+
+  //read fasta with reads and scores of their alignment with transcript
+  gzFile align_fasta;
+
+  align_fasta =
+      gzopen("example/alignment.fa", "r"); 
+  seq = kseq_init(align_fasta);  
+  while ((file_line = kseq_read(seq)) >= 0) { 
+  		cout << seq->name.s << endl;
+  		cout << seq->comment.l << endl;
+  		cout << "seq: " << seq->seq.s << endl;
+  }
+  printf("return value: %d\n", file_line);
+  kseq_destroy(seq);  
+  gzclose(align_fasta); 
+
+
 
   return 0;
 }
