f6e168757f31043e110f7e3bd82125a6198a7a5b
Tamara <t.ceccato@campus.unimib.it>
2018-12-27 22:57:46 +0100

    add option for 2 reads file

--- main.cpp
+++ main.cpp
@@ -30,17 +30,6 @@ void print_menu() {
   cout << "Choice: ";
 }
 
-char *getCmdOption(char **begin, char **end, const std::string &option) {
-  char **itr = std::find(begin, end, option);
-  if (itr != end && ++itr != end) {
-    return *itr;
-  }
-  return 0;
-}
-
-bool cmdOptionExists(char **begin, char **end, const std::string &option) {
-  return std::find(begin, end, option) != end;
-}
 
 // STEP 1: declare the type of file handler and the read() function
 KSEQ_INIT(gzFile, gzread)
@@ -62,7 +51,7 @@ int main(int argc, char *argv[]) {
   bool double_argument = false;
   string transcript_name;
   string read_name;
-  string transcript_name2;
+  string read_name2;
 
 
   time_t start = time(0);
@@ -71,13 +60,13 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
     	for(int i =0; i < argc; i++){
     	string option = argv[i];
-    		if (option == "-s1")
-    			transcript_name = argv[i+1];
     		if (option == "-r")
+    			transcript_name = argv[i+1];
+    		if (option == "-r1")
     			read_name = argv[i+1];
-    		if (option == "-s2"){
+    		if (option == "-r2"){
     			double_argument = true;
-    			transcript_name2 = argv[i+1];
+    			read_name2 = argv[i+1];
     		}
     	}
     } else {
@@ -163,45 +152,7 @@ int main(int argc, char *argv[]) {
   std::cerr << "return value: " << file_line << std::endl;
   kseq_destroy(seq);        // STEP 5: destroy seq
   gzclose(transcript_file); // STEP 6: close the file handler
-  cout << mapped_ID << endl;
-  if (double_argument) {
-    transcript_file =
-        gzopen(transcript_name2.c_str(), "r"); // STEP 2: open the file handler
-    seq = kseq_init(transcript_file);         // STEP 3: initialize seq
-    string input_seq;
-    // open and read the .fa
-    while ((file_line = kseq_read(seq)) >=
-           0) { // STEP 4: read sequence of transcript
-
-      // seq name is the key map for the transcript, and has an assigned int
-      input_seq = seq->seq.s;
-      //  std::cerr << name_transcript << std::endl;
-
-      if (input_seq.size() >= kmer_length) {
-        legend_ID[mapped_ID] = seq->name.s;
-        // split each sequence of a transcritp in k-mer with k=n
-
-        transcript_kmers_vec.resize(input_seq.size() - (kmer_length - 1));
-        transform(input_seq.cbegin(), input_seq.cend() - (kmer_length - 1),
-                  transcript_kmers_vec.begin(), [kmer_length](const auto &i) {
-                    return string(&i, kmer_length);
-                  });
-
-        // add all k-mers to BF
-
-        for (auto &kmer : transcript_kmers_vec) {
-          bloom.add_kmer(kmer);
-        }
-        mapped_ID++;
-
-        transcript_kmers_vec.clear();
-      }
-      input_seq.clear();
-    }
-    std::cerr << "return value: " << file_line << std::endl;
-    kseq_destroy(seq);        // STEP 5: destroy seq
-    gzclose(transcript_file); // STEP 6: close the file handler
-  }
+  
 
   std::cerr << "Transcript file processed" << std::endl;
   cout << mapped_ID << endl;
@@ -250,39 +201,8 @@ int main(int argc, char *argv[]) {
   std::cerr << "return value: " << file_line << std::endl;
   kseq_destroy(seq);        // STEP 5: destroy seq
   gzclose(transcript_file); // STEP 6: close the file handler
-  cout << idx << endl;
-  if (double_argument) {
-    transcript_file = gzopen(transcript_name2.c_str(), "r");
-    seq = kseq_init(transcript_file);
 
-    // open and read the .fa, every time a kmer is found the relative index is
-    // added to BF
-    while ((file_line = kseq_read(seq)) >= 0) {
-      input_seq = seq->seq.s;
-
-      if (input_seq.size() >= kmer_length) {
-
-        // split each sequence of a transcritp in k-mer with k=n
-
-        transcript_kmers_vec.resize(input_seq.size() - (kmer_length - 1));
-        transform(input_seq.cbegin(), input_seq.cend() - (kmer_length - 1),
-                  transcript_kmers_vec.begin(), [kmer_length](const auto &i) {
-                    return string(&i, kmer_length);
-                  });
-
-        // add for each k-mer its id to BF
-        for (auto &kmer : transcript_kmers_vec)
-          bloom.add_to_kmer(kmer, idx);
-
-        idx++;
-      }
-    }
-
-    std::cerr << "return value: " << file_line << std::endl;
-    kseq_destroy(seq);        // STEP 5: destroy seq
-    gzclose(transcript_file); // STEP 6: close the file handler
-  }
-
+  
   std::cerr << "Transcript indexes added to Bloom filter" << std::endl;
   time_t end_add_time = time(0);
   int time_adding = difftime(end_add_time, add_time) * 1000.0;
@@ -292,7 +212,7 @@ int main(int argc, char *argv[]) {
 
   std::cerr << "Time to add indexes:  " << minutes << ":" << seconds
             << std::endl;
-  cout << idx << endl;
+
   bloom.switch_mode(2);
 
   time_t association_time = time(0);
@@ -305,7 +225,7 @@ int main(int argc, char *argv[]) {
   //   FILE * pFile;
   //   pFile = fopen ("id_results_test.tsv", "w");
   ofstream file;
-  file.open("id_test.tsv");
+  file.open("id_results.tsv");
 
   // open .fq file that contains the reads
   read_file = gzopen(read_name.c_str(), "r"); // STEP 2: open the file handler
@@ -366,11 +286,73 @@ int main(int argc, char *argv[]) {
   }
 
   //  fclose(pFile);
-  file.close();
 
   std::cerr << "return value: " << file_line << std::endl;
   kseq_destroy(seq);  // STEP 5: destroy seq
   gzclose(read_file); // STEP 6: close the file handler
+
+
+if(double_argument){
+// open .fq file that contains the reads
+  read_file = gzopen(read_name2.c_str(), "r"); // STEP 2: open the file handler
+  seq = kseq_init(read_file);                 // STEP 3: initialize seq
+
+  while ((file_line = kseq_read(seq)) >= 0) { // STEP 4: read sequence of reads
+
+    read_seq = seq->seq.s;
+
+    if (read_seq.size() >= kmer_length) {
+
+      read_kmers_vec.resize(read_seq.size() - (kmer_length - 1));
+      transform(read_seq.cbegin(), read_seq.cend() - (kmer_length - 1),
+                read_kmers_vec.begin(), [kmer_length](const auto &i) {
+                  return string(&i, kmer_length);
+                });
+
+      // search all kmers of each read in the BF and store relative indexes
+
+      for (auto &kmer : read_kmers_vec) {
+        id_kmer = bloom.get_index(kmer);
+        //		int* buffer = &id_kmer[0];
+        //		fwrite (buffer, sizeof(int), sizeof(buffer), pFile);
+        while (id_kmer.has_next())
+          classification_id[id_kmer.get_next()]++;
+      }
+    }
+
+    int max = std::max(std::max_element(begin(classification_id),
+                                        end(classification_id),
+                                        [](const std::pair<int, int> &a,
+                                           const std::pair<int, int> &b) {
+                                          return a.second < b.second;
+                                        })
+                           ->second,
+                       (int)3);
+    // save in file all headers of transcripts probably associated to the read
+    // search and store in a file all elements of classification with
+    // max(classification[i])
+    for (auto it_class = classification_id.cbegin();
+         it_class != classification_id.cend(); it_class++) {
+      if (it_class->second == max && it_class->second >= threshold) {
+        // legend_ID[it_class->first] is the name of the transcript, mapped
+        /// with index it_class->first
+        file << seq->name.s << "\t" << (legend_ID.at(it_class->first)).c_str()
+             << "\n";
+        //       fwrite(seq->name.s, 1, seq->name.l, pFile);
+        //       fwrite("\t", 1, sizeof("\t"), pFile);
+        //       fwrite((legend_ID.at(it_class->first)).c_str(), 1,
+        //       strlen((legend_ID.at(it_class->first)).c_str()), pFile);
+        //       fwrite("\n", 1, sizeof("\n"), pFile);
+        //       fflush(pFile);
+      }
+    }
+    classification_id.clear();
+    id_kmer.clear();
+    read_kmers_vec.clear();
+  }
+
+}
+  file.close();
 
   std::cerr << "Association done." << std::endl;
   time_t end_assoc_time = time(0);
