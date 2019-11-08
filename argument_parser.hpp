c4aab0d32b1b97ddaf89dbfc9adeccad08000464
Marco Previtali <marco.previtali@unimib.it>
2019-11-08 15:44:53 +0100

    Add parameters checks

--- argument_parser.hpp
+++ argument_parser.hpp
@@ -11,10 +11,10 @@ static const char *USAGE_MESSAGE =
   "\n"
   "      -h, --help                        display this help and exit\n"
   "      -r, --reference                   reference sequences in FASTA format (can be gzipped)\n"
-  "      -1, --sample1                     sample in FASTA/Q (can be gzipped)\n"
-  "      -2, --sample2                     second sample in FASTA/Q (optional, can be gzipped)\n"
-  "      -o, --out1                        first output sample in FASTA/Q (default: sharked_sample.1)\n"
-  "      -p, --out2                        second output sample in FASTA/Q (default: sharked_sample.2)\n"
+  "      -1, --sample1                     sample in FASTQ (can be gzipped)\n"
+  "      -2, --sample2                     second sample in FASTQ (optional, can be gzipped)\n"
+  "      -o, --out1                        first output sample in FASTQ (default: sharked_sample.1)\n"
+  "      -p, --out2                        second output sample in FASTQ (default: sharked_sample.2)\n"
   "      -k, --kmer-size                   size of the kmers to index (default:17)\n"
   "      -c, --confidence                  confidence for associating a read to a gene (default:0.6)\n"
   "      -b, --bf-size                     bloom filter size in GB (default:1)\n"
@@ -28,8 +28,8 @@ namespace opt {
   static std::string fasta_path = "";
   static std::string sample1_path = "";
   static std::string sample2_path = "";
-  static std::string out1_path = "sharked_sample.1";
-  static std::string out2_path = "sharked_sample.2";
+  static std::string out1_path = "";
+  static std::string out2_path = "";
   static bool paired_flag = false;
   static uint k = 17;
   static double c = 0.6;
@@ -68,6 +68,12 @@ void parse_arguments(int argc, char **argv) {
       break;
     case 't':
       arg >> opt::nThreads;
+      if(opt::nThreads <= 0) {
+        std::cerr << "USAGE_MESSAGE";
+        std::cerr << "shark: at least 1 thread is required." << std::endl
+                  << "aborting..." << std::endl;
+        exit(EXIT_FAILURE);
+      }
       break;
     case '1':
       arg >> opt::sample1_path;
@@ -84,9 +90,20 @@ void parse_arguments(int argc, char **argv) {
       break;
     case 'k':
       arg >> opt::k;
+      if(opt::k == 0 or opt::k > 31) {
+        std::cerr << USAGE_MESSAGE;
+        std::cerr << "shark: k must be in the range [1, 31]." << std::endl
+                  << "aborting..." << std::endl;
+        exit(EXIT_FAILURE);
+      }
       break;
     case 'c':
       arg >> opt::c;
+      if(opt::c < 0 or opt::c > 1) {
+        std::cerr << "shark: c must be in the range [0, 1]." << std::endl
+                  << "aborting..." << std::endl;
+        exit(EXIT_FAILURE);
+      }
       break;
     case 'b':
       // Let's consider this as GB
@@ -96,6 +113,12 @@ void parse_arguments(int argc, char **argv) {
     case 'q':
       int mq;
       arg >> mq;
+      if(mq < 0) {
+        std::cerr << USAGE_MESSAGE;
+        std::cerr << "shark: q must be a positive value." << std::endl
+                  << "aborting..." << std::endl;
+        exit(EXIT_FAILURE);
+      }
       opt::min_quality = static_cast<char>(mq);
       break;
     case 's':
@@ -118,6 +141,13 @@ void parse_arguments(int argc, char **argv) {
     std::cerr << "shark : missing required files" << std::endl;
     std::cerr << "\n" << USAGE_MESSAGE;
     exit(EXIT_FAILURE);
+  }
+
+  if(opt::out1_path == "") {
+    opt::out1_path = "sharked_sample.1";
+  }
+  if(opt::out2_path == "" and opt::sample2_path != "") {
+    opt::out2_path = "sharked_sample.2";
   }
 }
 
