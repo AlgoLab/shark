e8de77491ba3e5b9a78ed485f483b725e5a0b0d1
Marco Previtali <marco.previtali@unimib.it>
2019-11-11 11:35:36 +0100

    Adds GPL3 header

--- kmer_utils.hpp
+++ kmer_utils.hpp
@@ -1,3 +1,24 @@
+/**
+ * shark - Mapping-free filtering of useless RNA-Seq reads
+ * Copyright (C) 2019 Tamara Ceccato, Luca Denti, Yuri Pirola, Marco Previtali
+ *
+ * This file is part of shark.
+ *
+ * shark is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * shark is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with shark; see the file LICENSE. If not, see
+ * <https://www.gnu.org/licenses/>.
+ **/
+
 #ifndef _KMER_UTILS_HPP
 #define _KMER_UTILS_HPP
 
